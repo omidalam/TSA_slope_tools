6652a894f387e5f254ce172859880949f262bc31
Omid Gholamalamdari <omid.alamdari@gmail.com>
2019-03-12 15:38:36 -0500

    Export functionality added

--- bw_perc.r
+++ bw_perc.r
@@ -1,7 +1,24 @@
 # make percentile out of bigwig files
-bw_perc<-function(bigwig_path){
-  bw<-import(bw_file_path)
+#Arguments
+# bigwig_path
+# cut_off <-percentile cutoff between 0 to 1
+bw_perc<-function(bigwig_path,cut_off=FALSE,bigwig_export=FALSE){
+  library(rtracklayer)
+  library(GenomeInfoDb)
+  bw<-import(bigwig_path)
   perc.rank <- function(x) trunc(rank(x))/length(x)
-  bw$score<-perc.rank(bw$score)
-  export(object = ,con = paste(tools::file_path_sans_ext(bw_file_path),"_pct.bw",sep=''))
-}
+  temp<-bw
+  temp$score<-perc.rank(bw$score)
+  cutoff_file_ext<-""
+  if (cut_off){
+    temp<-temp[temp$score>cut_off]
+    cutoff_file_ext<-trunc(cut_off*100)
+  }
+  if (bigwig_export){
+      export(object = temp,con = paste(tools::file_path_sans_ext(bigwig_path),"_",
+                                       cutoff_file_ext,"pct.bw",sep='')
+      )
+    }else return(temp)
+    
+    
+  }
